6964715c4801632137401b17ccda0bfbede27e5f
Masud Rana <masud.rana@apsissolutions.com>
2024-11-01 17:19:47 +0100

    add department features

--- department-service/src/main/java/com/project/departmenService/controller/DepartmentController.java
+++ department-service/src/main/java/com/project/departmenService/controller/DepartmentController.java
@@ -1,9 +1,30 @@
 package com.project.departmenService.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.project.departmenService.model.Department;
+import com.project.departmenService.repository.DepartmentRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/department")
 public class DepartmentController {
+
+    private static final Logger LOG = LoggerFactory.getLogger(DepartmentController.class);
+
+    @Autowired
+    private DepartmentRepository departmentRepository;
+
+    @PostMapping
+    public Department addDepartment(@RequestBody Department department){
+        return departmentRepository.addDepartment(department);
+    }
+
+    @GetMapping
+    public List<Department> getDepartment (){
+        return departmentRepository.findAll();
+    }
 }
